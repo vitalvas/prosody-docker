458f9791f78d5d889c1db4b600d9b8d44541fc9d
Vitaliy Vasylenko <me@vitalvas.com>
2023-12-14 12:16:49 -0500

    feat: add sqlite3

--- build.sh
+++ build.sh
@@ -11,6 +11,6 @@ apt install -qy wget mercurial
 wget -O /etc/apt/sources.list.d/prosody.sources https://prosody.im/files/prosody.sources
 apt update -qy
 
-apt install -y --no-install-recommends prosody lua-unbound lua-event lua-zlib lua-dbi-sqlite3 lua-dbi-postgresql lua-bitop ca-certificates ssl-cert
+apt install -y --no-install-recommends prosody lua-unbound lua-event lua-zlib lua-dbi-sqlite3 lua-dbi-postgresql lua-bitop ca-certificates ssl-cert sqlite3
 
 hg clone https://hg.prosody.im/prosody-modules/ /opt/prosody-modules
